ab21d952547571ff55f48abcaf1b961e2abe5550
Victor Hernandez <vik407@gmail.com>
2019-12-10 14:19:18 -0500

    0. Positive anything is better than negative nothing 0x2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(repr(number)[-1])
 str = "Last digit of "
+last = number % 10
+if number < 0:
+    last -= 10
 if last > 5:
     print(str+"{:d} is {:d} and is greater than 5".format(number, last))
 elif last == 0:
