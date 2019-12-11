1a9748606e975e63ab570ce4b2f90a3154869761
Victor Hernandez <vik407@gmail.com>
2019-12-10 23:12:38 -0500

    13. Insert in sorted linked list 0x1

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,35 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
- * insert_node - function in C that inserts a number into a sorted singly linked list
+ * insert_node - function to insert a number into a sorted singly linked list
  * @head: head of the link list
  * @number: number
  * Return: the address of the new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new = NULL, *prev = NULL, *current = *head;
+	listint_t *new = NULL, *prev = NULL, *current = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-    
-    if (new)
-    {
-        new->n = number;
-        new->next = current;
-        if (new->n > new->next->n || new->next == NULL)
-            current = new;
-        else
-        {
-            do
-            {
-                prev = new->next;
-                new->next = prev->next;
-            } while (new->next && new->n > new->next->n);
-            
-        }
-        if (prev)
-            prev->next = new;
-    }
-    return (new);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (new)
+	{
+		new->n = number;
+		new->next = current;
+		if (new->n <= current->n || !new->next)
+			current = new;
+		else
+		{
+			do {
+				prev = new->next;
+				new->next = prev->next;
+			} while (new->next && new->n > new->next->n);
+		}
+		if (prev)
+			prev->next = new;
+	}
+	return (new);
 }
