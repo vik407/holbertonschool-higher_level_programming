3926f6a05053eb598e292ace875682e272d23126
Victor Hernandez <vik407@gmail.com>
2019-12-17 23:28:48 -0500

    13. Linked list palindrome 0x0

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * is_palindrome - Write a function in C that checks if
+ * a singly linked list is a palindrome.
+ * @head: head to the linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *_head;
+	int num_arr[3000];
+	int i = 0, j = 0;
+
+	if (!head || !(*head) || !((*head)->next))
+		return (1);
+
+	_head = *head;
+
+	while (_head)
+	{
+		num_arr[i] = _head->n;
+		i++;
+		_head = _head->next;
+	}
+	i--;
+	while (j < i)
+	{
+		if (num_arr[i] == num_arr[j])
+			continue;
+		else
+			return (0);
+		j++;
+		i--;
+	}
+	return (1);
+}
