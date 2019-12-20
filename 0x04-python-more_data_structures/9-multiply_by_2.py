6e61dbd2c228fc776d65ac2c587513172691fb6a
Victor Hernandez <vik407@gmail.com>
2019-12-19 23:23:57 -0500

    9. Multiply by 2 0x3

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     _a_dictionary = {}
-    for key, val in a_dictionary:
+    for key, val in a_dictionary.items():
         _a_dictionary[key] = val * 2
     return a_dictionary
