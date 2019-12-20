ef7ae8927692bed060bb5263f1e99c07678e92e0
Victor Hernandez <vik407@gmail.com>
2019-12-19 21:41:02 -0500

    1. Search and replace 0x1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,6 @@ def search_replace(my_list, search, replace):
     for m in my_list:
         if m == search:
             _my_list.append(replace)
-        _my_list.append(m)
+        else:
+            _my_list.append(m)
     return _my_list
