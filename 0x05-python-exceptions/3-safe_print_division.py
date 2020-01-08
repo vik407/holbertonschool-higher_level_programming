23330e65191f98b50ae697fd0020714abcf66cd5
Victor Hernandez <vik407@gmail.com>
2020-01-08 21:08:27 +0000

    3. Integers division with debug 0x0

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
-    return
+    equals = None
+    try:
+        equals = a / b
+    except:
+        pass
+    finally:
+        if equals is not None:
+            print("Inside result: {:.1f}".format(equals))
+        else:
+            print("Inside result: None")
+    return equals
