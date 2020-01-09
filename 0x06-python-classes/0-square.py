015f17a07ade2785949b6aaf2dbc2f6633efdc5c
Victor Hernandez <vik407@gmail.com>
2020-01-09 23:32:40 +0000

    1. Square with size 0x2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class Square:
+    """Square - an empty class Square that defines a square"""
     None
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 class Square:
+    """Square - a class Square that defines a square by"""
     def __init__(self, size=0):
         self.__size = size
