4dbd26d1f60ae180625afbd04c7eb8247a5675dd
Victor Hernandez <vik407@gmail.com>
2020-01-11 21:24:26 +0000

    0. Integers addition 0x0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+add_integer - a function that adds 2 integers.
+Returns an integer: the addition of a and b
+"""
+
+
+def add_integer(a, b=98):
+    """
+    @a = integers or floats
+    @b = integers or floats (initialized in 98)
+    """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)) is False:
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))
