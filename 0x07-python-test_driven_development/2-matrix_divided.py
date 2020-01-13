894752438c5650f7b83f8b40d6dcd4ef600b29e2
Victor Hernandez <vik407@gmail.com>
2020-01-13 21:56:02 +0000

    say my name 0x0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,14 +14,20 @@ def matrix_divided(matrix, div):
     msg_2 = "Each row of the matrix must have the same size"
     msg_3 = "div must be a number"
     msg_4 = "division by zero"
+    equals = []
     if isinstance(matrix, (list)) is None or matrix is False:
         raise TypeError(msg_1)
+    if isinstance(div, (int, float)) is False:
+        raise TypeError(msg_3)
+    if div is 0:
+        raise TypeError(msg_4)
+    if(len(set([len(_list) for _list in matrix])) > 1):
+        raise TypeError(msg_2)
     for row in matrix:
         if isinstance(row, list) is False:
             raise TypeError(msg_1)
         for a in row:
             if isinstance(a, (int, float)) is False:
                 raise TypeError(msg_1)
-
-        raise TypeError("b must be an integer")
-    return(int(a) + int(b))
+    equals = [[round(num / div, 2) for num in row] for row in matrix]
+    return equals
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+matrix_divided - a function that prints My name is <first name> <last name>
+Returns <first name> <last name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    @first_name = str with firstname value
+    @last_name = str with last_name value
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
