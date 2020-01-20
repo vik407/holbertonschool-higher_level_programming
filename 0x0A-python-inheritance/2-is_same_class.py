46c670a96893cf3de775539cccce97020892914d
Victor Hernandez <vik407@gmail.com>
2020-01-20 22:48:52 +0000

    3. Same class or inherit from 0x0

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """2. Exact same object
 """
+
+
 def is_same_class(obj, a_class):
     """a function that returns True if the object is exactly an
     instance of the specified class ; otherwise False.
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""3. Same class or inherit from
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """A function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    """
+
+    return isinstance(obj, a_class)
