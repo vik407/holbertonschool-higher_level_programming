f56417be57f64480fc69986081b684dfa6a785bf
Victor Hernandez <vik407@gmail.com>
2020-01-20 23:54:59 +0000

    9. Full rectangle 0x0

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""8. Rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """Instantiation with width and height
+           width and height must be private. No getter or setter
+           width and height must be positive integers, validated
+           by integer_validator
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Definition of area method"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return string representation"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
