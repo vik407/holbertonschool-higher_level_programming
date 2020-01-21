48696898fc1bf348c93dce9053c29342d5f1231d
Victor Hernandez <vik407@gmail.com>
2020-01-21 00:25:49 +0000

    11. Square #2 0x0

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""11. Square #2
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Write a class Square that inherits from Rectangle (9-rectangle.py):
+    """
+
+    def __init__(self, size):
+        """size must be private. No getter or setter
+           size must be a positive integer, validated by integer_validator
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """the area() method must be implemented
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+            """print() should print, and str() should return, the square
+            description: [Square] <width>/<height>"""
+            return "[Square] " + str(self.__size) + "/" + str(self.__size)
