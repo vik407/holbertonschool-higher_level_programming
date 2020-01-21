20859eb3a36bcd09080e8158a216c2df954d538b
Victor Hernandez <vik407@gmail.com>
2020-01-21 21:46:49 +0000

    5. To JSON string 0x0

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""4. Append to a file
+"""
+
+
 def append_write(filename="", text=""):
+    """Write a function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added:
+    """
+    with open(filename, 'a') as file:
+        file_write = file.write(text)
+    return file_write
--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""5. To JSON string
+"""
+import json
+
+
 def to_json_string(my_obj):
+    """Write a function that returns the JSON representation
+    of an object (string):
+    """
+    return json.dumps(my_obj)
