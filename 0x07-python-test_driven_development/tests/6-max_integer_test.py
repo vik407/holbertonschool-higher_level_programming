0ea0cf3b537f37ab578feb453cab294ebf2f000a
Victor Hernandez <vik407@gmail.com>
2020-01-21 04:00:29 +0000

    Solution max integer test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """Unittest for max_integer([..])
     """
 
-    def cases(self):
-        '''Float test'''
-        self.assertEqual([1, 2.3, 3, 4], 4)
-        self.assertEqual([1, 2, 3, 4], 4)
-        self.assertEqual([-1, -2, -3, -4], -1)
-        self.assertEqual([4, 3, 2, 1], 4)
-        self.assertEqual([-4, -3, -2, -1], -1)
-        self.assertEqual([10], 10)
-        self.assertEqual([], None)
+    def test_cases_1(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_cases_2(self):
+        self.assertEqual(max_integer([1, 2, 0x3b76, 4]), 15222)
+
+    def test_cases_3(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_cases_4(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_cases_5(self):
+        self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
+
+    def test_cases_6(self):
+        self.assertEqual(max_integer([10]), 10)
+
+    def test_cases_7(self):
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
