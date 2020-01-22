838da0d917b48c5a96c0ed0960f5a7c0467b8a2d
Victor Hernandez <vik407@gmail.com>
2020-01-22 02:08:09 +0000

    11. Student to JSON 0x0

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""11. Student to JSON
+"""
+
+
+class Student:
+    """Write a class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Set public instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student
+        """
+        return self.__dict__
