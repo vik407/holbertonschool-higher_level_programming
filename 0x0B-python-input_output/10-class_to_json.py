4253bc78ce75cca0eddb196b5b30ac94edf6440f
Victor Hernandez <vik407@gmail.com>
2020-01-22 02:01:05 +0000

    10. Class to JSON 0x1

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -6,9 +6,10 @@
 def class_to_json(obj):
     """Write a function that returns the dictionary description with simple
     data structure (list, dictionary, string, integer and boolean) for JSON
-    serialization of an object:
+    serialization of an object.
     """
-    if hasattr(obj, "__dict__")
+    if hasattr(obj, "__dict__"):
         return obj.__dict__
-    if hasattr(obj, "__slots__")
+
+    if hasattr(obj, "__slots__"):
         return obj.__slots__
