3314e1177ed94ea9e1245c0d8691c5d56d00b7dc
Victor Hernandez <vik407@gmail.com>
2020-01-22 00:25:14 +0000

    6. From JSON string to Object 0x0

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""6. From JSON string to Object
+"""
+import json
+
+
 def from_json_string(my_str):
+    """Write a function that returns an object (Python data structure)
+    represented by a JSON string:
+    """
+    return json.loads(my_str)
