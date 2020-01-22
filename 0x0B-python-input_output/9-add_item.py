332a18f5e1e6a241cf4eb54b1bb0ea2734495524
Victor Hernandez <vik407@gmail.com>
2020-01-22 01:46:46 +0000

    9. Load, add, save 0x0

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""9. Load, add, save
+Write a script that adds all arguments to a Python list, and then save
+them to a file:
+"""
+import os.path
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+arguments = sys.argv[1:]
+list = []
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    list = load_from_json_file(filename)
+
+save_to_json_file(list + arguments, filename)
