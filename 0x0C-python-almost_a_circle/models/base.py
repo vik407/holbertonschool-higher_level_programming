09b928d02367cdaab0d254748731a52c5e3de9ea
Victor Hernandez <vik407@gmail.com>
2020-01-23 17:30:01 +0000

    1. Base class 0x0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""This is the main file for the base class
+"""
+
+
+class Base:
+    """Base class for Holberton's 0x0C. Python - Almost a circle project
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of the class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
