215bb3fedf27f56199405d0f7b6088db31713809
Victor Hernandez <vik407@gmail.com>
2020-01-27 18:58:15 +0000

    8. Update #0 0x0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,16 @@ class Rectangle(Base):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
+
+    def update(self, *args, **kwargs):
+        """Adding the public method def update(self, *args): that assigns an
+        argument to each attribute: id, width, height, x, y.
+        """
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, e in enumerate(args):
+                setattr(self, attrs[i], e)
+            return
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
