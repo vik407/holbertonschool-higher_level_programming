32ce7fec17b97eae7a581d23abb35e2299dad3ae
Victor Hernandez <vik407@gmail.com>
2020-01-28 02:13:39 +0000

    16. JSON string to file 0x0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """This is the main file for the base class
 """
 import json
+import csv
+import os.path
 
 
 class Base:
@@ -30,3 +32,20 @@ class Base:
         if any(type(s) != dict for s in list_dictionaries):
             raise TypeError("list_dictionaries must contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file
+        """
+        if type(list_objs) != list and list_objs is not None:
+            raise TypeError("list_objs must be a list")
+        if list_objs is None or list_objs == []:
+            output = []
+        else:
+            first = type(list_objs[0])
+            if any(type(s) != first for s in list_objs):
+                raise ValueError("all elements of list_objs must match")
+            output = [c.to_dictionary() for c in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(output))
