66a93bf25b53e44a6d94e6eb3c2a64149945829e
Victor Hernandez <vik407@gmail.com>
2020-03-04 05:13:05 +0000

    Fix sub query not in

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,7 +1,7 @@
 -- A script that uses the hbtn_0d_tvshows database to list all genres
--- not linked to the show Dexter
-SELECT DISTINCT tv_genres.name
-FROM tv_genres
+-- Fields
+SELECT DISTINCT tv_genres.name FROM tv_genres
+-- Condition not in subquery
 WHERE tv_genres.name NOT IN
 (SELECT tv_genres.name FROM tv_shows
 INNER JOIN tv_show_genres
@@ -9,4 +9,5 @@ ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter')
+-- Order data
 ORDER BY tv_genres.name ASC;
