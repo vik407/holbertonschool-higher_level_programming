f6900832c40524aa5efa3ced19c8307de0472f8e
Victor Hernandez <vik407@gmail.com>
2020-03-28 23:43:25 +0000

    Other type of formatting the document x 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 # Write a script that lists all states from the database hbtn_0e_0_usa
 
-import MySQLdb
-from sys import argv
-
-
 def main():
     """Get variables, connect to mysql and run the query """
+    from sys import argv
+    import MySQLdb
+
+    username = str(argv[1])
+    password = str(argv[2])
+    db_name = str(argv[3])
+
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         charset="utf8")
+                         port = 3306,
+                         user = username,
+                         passwd = password,
+                         db = db_name,
+                         charset = "utf8")
     con = db.cursor()
-    con.execute("SELECT * FROM states ORDER BY states.id ASC")
+    con.execute("SELECT * FROM states")
     rows = con.fetchall()
     for row in rows:
         print(row)
