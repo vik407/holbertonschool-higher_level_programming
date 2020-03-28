8409f998323a26ce2bcd01256b6c28b2d5283640
Victor Hernandez <vik407@gmail.com>
2020-03-28 23:59:27 +0000

    Refactory again, works but checker says..

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa
 
-
-def main():
-    """Get variables, connect to mysql and run the query """
-    from sys import argv
-    import MySQLdb
-
-    username = str(sys.argv[1])
-    password = str(sys.argv[2])
-    db_name = str(sys.argv[3])
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=db_name, charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
-
+"""Write a script that lists all states from the database hbtn_0e_0_usa
+"""
 
 if __name__ == '__main__':
-    """Run it if main"""
-    main()
+
+    from sys import argv
+
+    if len(argv) > 3:
+        import MySQLdb
+
+        username = argv[1]
+        password = argv[2]
+        db_name = argv[3]
+        conn = MySQLdb.connect(
+            host="localhost", port=3306, user=username,
+            passwd=password, db=db_name, charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
