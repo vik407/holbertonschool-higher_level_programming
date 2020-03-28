280cf5e94e5666431f4580d06331af10a9cb76cc
Victor Hernandez <vik407@gmail.com>
2020-03-28 23:15:41 +0000

    Identation ...

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,17 +9,17 @@ if __name__ == '__main__':
     mysql_psw = sys.argv[2]
     mysql_db = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=mysql_usr,
-                        passwd=mysql_psw,
-                        db=mysql_db,
-                        charset="utf8")
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_usr,
+                         passwd=mysql_psw,
+                         db=mysql_db,
+                         charset="utf8")
 
-conn = db.cursor()
-conn.execute("SELECT * FROM states ORDER BY id ASC")
-rows = conn.fetchall()
-for row in rows:
-    print(row)
-conn.close()
-db.close()
+    conn = db.cursor()
+    conn.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = conn.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+    db.close()
