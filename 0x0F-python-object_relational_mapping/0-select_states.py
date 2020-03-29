f2a03277e01fe6635ced0aa50a19666d59f9bc87
Victor Hernandez <vik407@gmail.com>
2020-03-29 02:15:14 +0000

    Lost hope with this one x3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database
-"""
+# Write a script that lists all states from the database hbtn_0e_0_usa
 
 
-import MySQLdb
-from sys import argv
+def main():
+    """Get variables, connect to mysql and run the query """
+    from sys import argv
+    import MySQLdb
 
-if __name__ == "__main__":
-    mysql_username = argv[1]
-    mysql_password = argv[2]
-    database_name = argv[3]
+    username = str(argv[1])
+    password = str(argv[2])
+    db_name = str(argv[3])
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=mysql_username, passwd=mysql_password,
-                         db=database_name, charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    all_rows = cur.fetchall()
-    for row in all_rows:
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         charset="utf8")
+    con = db.cursor()
+    con.execute("SELECT * FROM states")
+    rows = con.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
+    con.close()
     db.close()
+
+
+if __name__ == '__main__':
+    """Run it if main"""
+    main()
