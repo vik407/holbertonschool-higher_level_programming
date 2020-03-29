398a5944e01890904bc28050139e737157ee3508
Victor Hernandez <vik407@gmail.com>
2020-03-29 19:52:52 +0000

    my safe filter finally

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+# Write a script that takes in an argument and displays all values in the
+# states table of hbtn_0e_0_usa where name matches the argument.
+
+
+def main():
+    """Get variables, connect to mysql and run the query """
+    from sys import argv
+    import MySQLdb
+
+    username = str(argv[1])
+    password = str(argv[2])
+    db_name = str(argv[3])
+    st_name = str(argv[4])
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         charset="utf8")
+    con = db.cursor()
+    con.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC;",
+                (st_name,))
+    rows = con.fetchall()
+    for row in rows:
+        print(row)
+    con.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    """Run it if main"""
+    main()
