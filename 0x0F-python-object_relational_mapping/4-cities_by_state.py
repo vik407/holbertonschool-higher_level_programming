dbb6a4614e9613cdd2fe6315f918085a279580f1
Victor Hernandez <vik407@gmail.com>
2020-03-29 19:59:49 +0000

    cities by state take one

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+# Write a script that takes in an argument and displays all values in the
+# states table of hbtn_0e_0_usa where name matches the argument.
+
+
+def main():
+    """Get variables, connect to mysql and run the query """
+    from sys import argv
+    import MySQLdb
+
+    username = str(argv[1])
+    password = str(argv[2])
+    db_name = str(argv[3])
+    st_name = str(argv[4])
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         charset="utf8")
+    con = db.cursor()
+    con.execute("SELECT cities.id, cities.name, states.name FROM cities " +\
+            "JOIN states ON cities.state_id = states.id ORDER BY id ASC;)
+    rows = con.fetchall()
+    for row in rows:
+        print(row)
+    con.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    """Run it if main"""
+    main()
