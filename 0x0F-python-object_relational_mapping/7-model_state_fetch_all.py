27e48451c4495d7db8a9fa6c07650d3fbe42cb27
Victor Hernandez <vik407@gmail.com>
2020-03-30 01:03:50 +0000

    fetch all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+# Lists all State objects from the database hbtn_0e_6_usa
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{:d}: {:s}".format(state.id, state.name))
+    session.close()
