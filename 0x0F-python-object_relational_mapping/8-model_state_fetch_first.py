029ff098d24bb8f0282aec54822fdaa35bd5be44
Victor Hernandez <vik407@gmail.com>
2020-03-30 01:54:27 +0000

    fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+# Write a script that prints the first State object from the database
+# hbtn_0e_6_usa
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, psw, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    ret = session.query(State).first()
+
+    if ret:
+        print("1: " + ret.name)
+    else:
+        print("Nothing")
+
+    session.close()
