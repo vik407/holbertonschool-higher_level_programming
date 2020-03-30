4a260ecc3c8bdb47461cbdd562a9420ec16f9b9a
Victor Hernandez <vik407@gmail.com>
2020-03-30 02:42:36 +0000

    new state name

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+# Write a script that prints the first State object from the database
+# hbtn_0e_6_usa
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    st_new = session.query(State).filter_by(id=2).first()
+    try:
+        st_new.name = "New Mexico"
+    except Exception:
+        pass
+
+    session.close()
