a2da30b13efb19eef11f403ab15bbd36054f674e
Victor Hernandez <vik407@gmail.com>
2020-04-08 21:44:56 +0000

    Tasks from container tested ok

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+# Get the methods that the server permits
+SERVER=$1
+curl -sI -X OPTIONS "$SERVER" | grep Allow | cut -d' ' -f1 --complement
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# script that takes in a URL as an argument, sends a GET request to the URL,
+# and displays the body of the response
+SERVER=$1
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$SERVER"
