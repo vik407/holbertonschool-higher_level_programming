d2589e825f5c6c41456aeb9625380ca8d3cb3405
Victor Hernandez <vik407@gmail.com>
2020-04-08 00:10:39 +0000

    0 get content lenght

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# a Bash script that takes in a URL, sends a request to that URL, and displays
+# the size of the body of the response
+SERVER=$1
+curl -sI "$SERVER" | grep Content-Length | cut -d' ' -f2
