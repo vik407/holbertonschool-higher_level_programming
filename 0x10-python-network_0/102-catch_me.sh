654dfe995b03c3bba81e501457f4cf7d8a5e3381
Victor Hernandez <vik407@gmail.com>
2020-04-09 01:19:57 +0000

    Catch me if you cant with proper params

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# Find the way to the correct response
+# You are not coming from HolbertonSchool response
+# You are not user_id=98 response
+curl -s -L -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
