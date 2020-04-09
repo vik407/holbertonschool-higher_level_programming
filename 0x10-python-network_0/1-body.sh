b66d3228c2e9abed02b0eb235ff94135f586ab23
Victor Hernandez <vik407@gmail.com>
2020-04-09 18:06:44 +0000

    Test if checker doesnt like multi line comments

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
+# A Bash script that displays the body of the response
 curl -s -L -X GET "$1"
