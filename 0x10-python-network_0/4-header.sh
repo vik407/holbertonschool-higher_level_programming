e25be312e56e680c0eb99cd19b658d36ca645b76
Victor Hernandez <vik407@gmail.com>
2020-04-09 18:11:27 +0000

    Checker format

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL,
-# and displays the body of the response
-SERVER=$1
-curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$SERVER"
+# script that sends a GET request to the URL
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
