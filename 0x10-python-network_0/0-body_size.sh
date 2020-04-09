fd821567d66356d3be1ebf63101f776496b4cf41
Victor Hernandez <vik407@gmail.com>
2020-04-09 18:03:10 +0000

    Checker validation requirement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response
-SERVER=$1
-curl -sI "$SERVER" | grep Content-Length | cut -d' ' -f2
+# a Bash script that show the Content-Lenght
+curl -sI "$1" | grep Content-Length | cut -d' ' -f2
