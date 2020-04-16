ebdaa613ebd3cf29add5db7f627d4d577cb647e2
Victor Hernandez <vik407@gmail.com>
2020-04-16 23:11:43 +0000

    X-Request solve fix 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Python script that takes in a URL, sends a request and get X-Request-Id var
 """
+
 from urllib import request
 from sys import argv
 
-
-with request.urlopen(argv[1]) as req:
-    print(req.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as res:
+        print(res.getheader("X-Request-Id"))
