3074aef148a5e06d64c0562df9480d22fbdc1e13
Victor Hernandez <vik407@gmail.com>
2020-04-16 20:43:51 +0000

    Network status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status
+"""
+from urllib import request
+
+
+with request.urlopen("https://intranet.hbtn.io/status") as req:
+    out = req.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(out)))
+    print("\t- content: {}".format(out))
+    print("\t- utf8 content: {}".format(out.decode('utf-8')))
