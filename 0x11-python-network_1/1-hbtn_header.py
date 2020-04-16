a083004975af6dc105f4410acaa94c040d89f950
Victor Hernandez <vik407@gmail.com>
2020-04-16 20:55:00 +0000

    X-Request solve fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response.
+""" Python script that takes in a URL, sends a request and get X-Request-Id var
 """
 from urllib import request
 from sys import argv
