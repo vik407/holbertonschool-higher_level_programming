e5bf3fb81744232e17c17ff3ab8b957423d20ffc
Victor Hernandez <vik407@gmail.com>
2020-04-17 00:33:56 +0000

    POST an email #1 0x0

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Python script that takes in a URL, sends a request to the URL and displays
 the value of the variable X-Request-Id in the response header
 """
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays the body
+of the response.
+"""
+
+from requests import post
+from sys import argv
+
+if __name__ == "__main__":
+    res = post(argv[1], data={'email': argv[2]})
+    print(res.text)
