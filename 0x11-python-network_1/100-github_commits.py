0850f310a0f9183293e8935284d4235b33735ff6
Victor Hernandez <vik407@gmail.com>
2020-04-17 01:31:53 +0000

    10. Time for an interview! 0x0

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Python script that takes 2 valuments in order to solve this challenge
+Number of contributions per user and commit
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    json = get("https://api.github.com/repos/{}/{}/commits".
+               format(argv[2], argv[1])).json()
+    contrib = 0
+    for val in json:
+        contrib += 1
+        name = val.get("commit").get("author").get("name")
+        sha = val.get("sha")
+        print("{}: {}".format(sha, name))
+        if contrib == 10:
+            break
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,8 +2,10 @@
 """Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+
 from requests import post
 from sys import argv
+
 if __name__ == "__main__":
     if len(argv) > 1:
         q = argv[1]
