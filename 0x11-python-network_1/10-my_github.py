836159e380f8fcb8fe24cdfce3b207a469805783
Victor Hernandez <vik407@gmail.com>
2020-04-17 01:26:47 +0000

    9. My Github! 0x0

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    req = get("https://api.github.com/user",
+              auth=(argv[1], argv[2])).json()
+    try:
+        print(req["id"])
+    except:
+        print(None)
