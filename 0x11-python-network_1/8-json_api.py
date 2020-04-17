7b3eca81786414d2338367046817b41de66c7e0e
Victor Hernandez <vik407@gmail.com>
2020-04-17 01:08:22 +0000

    8. Search API 0x0

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+from requests import post
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+    try:
+        request = post("http://0.0.0.0:5000/search_user",
+                       data={'q': q}).json()
+        if "id" in request and "name" in request:
+            print("[{}] {}".format(r["id"], r["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
