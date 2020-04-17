8f2eca80056b641fa62bf14b569345c49ce852df
Victor Hernandez <vik407@gmail.com>
2020-04-17 01:00:35 +0000

    7. Error code #1 0x0

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+"""
+
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    request = get(argv[1])
+    if request.status_code >= 400:
+        print("Error code: {}".format(request.status_code))
+    else:
+        print(request.text)
