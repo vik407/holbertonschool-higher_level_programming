a32726fbaa032e99cbca3a9a406e7c7250f93a6f
Victor Hernandez <vik407@gmail.com>
2020-04-17 00:19:09 +0000

    4. What's my status? #1 0x0

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+"""
+
+from requests import get
+
+if __name__ == "__main__":
+    res = get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
