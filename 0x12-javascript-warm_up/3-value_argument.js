256e43df6c27cfca0881680d8ad0eba8b9f27ce0
Victor Hernandez <vik407@gmail.com>
2020-05-05 20:23:13 +0000

    3. Value of my argument 0x0

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// A script that prints a message depending of the number of arguments passed
+const arg = process.argv[2];
+if (arg) {
+  console.log(arg);
+} else {
+  console.log('No argument');
+}
