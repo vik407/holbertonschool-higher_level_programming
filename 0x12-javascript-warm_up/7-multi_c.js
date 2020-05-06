306de1d7bafff9b601855a371c4fc92b4557ca7e
Victor Hernandez <vik407@gmail.com>
2020-05-06 02:15:11 +0000

    a script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// A script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+const arg = process.argv[2];
+
+if (Number.isInteger(Number(arg))) {
+  for (let i = 0; i < Number(arg); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
