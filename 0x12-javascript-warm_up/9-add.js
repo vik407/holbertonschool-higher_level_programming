a43a85195d925aa4b80c232ed7428722673df292
Victor Hernandez <vik407@gmail.com>
2020-05-06 02:34:24 +0000

    9. Add 0x0

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// a script that prints the addition of 2 integers
+
+const a = process.argv[2]; const b = process.argv[3];
+
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(Number(a) + Number(b));
+}
