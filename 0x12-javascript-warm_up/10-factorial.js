5030a23332835b73affc324c8d9c504092fb8b65
Victor Hernandez <vik407@gmail.com>
2020-05-06 03:26:41 +0000

    10. Factorial 0x1 to recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -10,13 +10,9 @@ if (isNaN(a)) {
 }
 
 function factorialize (num) {
-  var result = num;
-  if (num === 0 || num === 1) {
+  if (num <= 1) {
     return 1;
+  } else {
+    return num * factorialize(num - 1);
   }
-  while (num > 1) {
-    num--;
-    result *= num;
-  }
-  return result;
 }
