f9ffa08d829248c4846b21704e9f97e464e0804c
Victor Hernandez <vik407@gmail.com>
2020-05-06 02:26:39 +0000

    8. Square 0x0

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// a script that prints a square
+
+const arg = process.argv[2];
+
+if (Number.isInteger(Number(arg))) {
+  const size = Number(arg);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
