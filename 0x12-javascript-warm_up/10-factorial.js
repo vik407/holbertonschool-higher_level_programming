6eee4c7a5f29a4685edf228478521b12def7e4ad
Victor Hernandez <vik407@gmail.com>
2020-05-06 03:24:22 +0000

    10. Factorial 0x0

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+// a script that computes and prints a factorial
+
+const a = Number(process.argv[2]);
+
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorialize(a));
+}
+
+function factorialize (num) {
+  var result = num;
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
