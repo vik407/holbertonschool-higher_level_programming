683daa1e2037a3d7ad09d6db17d1bfd9da039ac1
Victor Hernandez <vik407@gmail.com>
2020-05-06 03:39:02 +0000

    11. Second biggest! 0x0

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// a script that searches the second biggest integer in the list of arguments.
+
+const a = process.argv.length;
+
+if (a <= 3) {
+  console.log(0);
+} else {
+  const nums = process.argv.slice(2);
+  nums.sort((a, b) => a - b);
+  console.log(Number(nums[nums.length - 2]));
+}
