c6a3b0635908a1bd6f9a85840ffd30bad96fe181
Victor Hernandez <vik407@gmail.com>
2020-05-06 00:59:10 +0000

    5. An Integer 0x0

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// A script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
+
+const arg = process.argv[2];
+
+if (isNaN(Number(arg))) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(arg, 10)}`);
+}
