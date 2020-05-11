a060fd33aec51fbb16458a5d91aafd4be3c970ad
Victor Hernandez <vik407@gmail.com>
2020-05-11 23:28:01 +0000

    Rectangle task 2 and 3 to print square

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+  // class methods
+  constructor (_width, _heigh) {
+    // constructor get width and height
+    if (isNaN(_width) || isNaN(_heigh) ||
+        _width <= 0 || _heigh <= 0) {
+      return;
+    }
+    this.width = _width;
+    this.height = _heigh;
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+  // class methods
+  constructor (_width, _heigh) {
+    // constructor get width and height
+    if (isNaN(_width) || isNaN(_heigh) ||
+        _width <= 0 || _heigh <= 0) {
+      return;
+    }
+    this.width = _width;
+    this.height = _heigh;
+  }
+
+  // Create an instance method called print() that prints the rectangle using the character X
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+}
+
+module.exports = Rectangle;
