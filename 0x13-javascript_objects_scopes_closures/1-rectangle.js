b28e4049dfb42f837001273062e5bd1521541faa
Victor Hernandez <vik407@gmail.com>
2020-05-11 23:09:09 +0000

    Class Rentangle get

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+  // class methods
+  constructor (_width, _heigh) {
+    // constructor get width and height
+    this.width = _width;
+    this.height = _heigh;
+  }
+}
+
+module.exports = Rectangle;
