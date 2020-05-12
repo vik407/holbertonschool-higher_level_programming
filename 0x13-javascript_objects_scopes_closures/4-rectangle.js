aedf40101c12c87edce649f1d5de62c6cfa6cc87
Victor Hernandez <vik407@gmail.com>
2020-05-12 01:28:04 +0000

    Rectangle andquare functions

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+// Write a class Rectangle that defines a rectangle:
+class Rectangle {
+  // class methods
+  constructor (_width, _heigh) {
+    // constructor get width and height
+    if (isNaN(_width) || isNaN(_heigh) ||
+        _width <= 0 || _heigh <= 0) {
+      return;
+    }
+    this.width = _width;
+    this.height = _heigh;
+  }
+
+  // Create an instance method called print() that prints the rectangle using the character X
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  // Create an instance method called rotate() that exchanges the width and the height of the rectangle
+  rotate () {
+    const __width = this.width;
+    this.width = this.height;
+    this.height = __width;
+  }
+
+  // Create an instance method called double() that multiples the width and the height of the rectangle by 2
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+// Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  // You must use the class notation for defining your class and extends
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
