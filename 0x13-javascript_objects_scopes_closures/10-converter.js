21a29241969f34794310cb472c5eb61933812c14
Victor Hernandez <vik407@gmail.com>
2020-05-12 02:15:16 +0000

    map factor index

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Write a function that converts a number from base 10 to another base passed as argument
+exports.converter = function (base) {
+  return function (num) {
+    // radix - base to use for representing a numeric value. Must be an integer between 2 and 36.
+    return num.toString(base);
+  };
+};
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+// Write a script that imports an array and computes a new array.
+const { list } = require('./100-data.js');
+
+console.log(list);
+// 1*0 / 2*1 / 3*2 / 4*3 / 5*4
+console.log(list.map((currVal, index) => currVal * index));
