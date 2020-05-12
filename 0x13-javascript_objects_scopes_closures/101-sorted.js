97209c2234f5f68c3b0e541f16b1d0af92c58728
Victor Hernandez <vik407@gmail.com>
2020-05-12 02:30:34 +0000

    Sorted 0x0

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+// Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+const { dict } = require('./101-data');
+
+console.log(Object.entries(dict).reduce((currVal, currIndex) => {
+  !currVal[currIndex[1]]
+    ? currVal[currIndex[1]] = [currIndex[0]]
+    : currVal[currIndex[1]].push(currIndex[0]);
+  return currVal;
+}, {}));
