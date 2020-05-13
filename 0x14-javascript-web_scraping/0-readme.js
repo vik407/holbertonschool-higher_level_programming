7383723e666b9d842d2acf9a300bfca566803cda
Victor Hernandez <vik407@gmail.com>
2020-05-13 00:55:49 +0000

    read files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+const fs = require('fs');
+
+fs.readFile(`${process.argv[2]}`, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString('utf8'));
+  }
+});
