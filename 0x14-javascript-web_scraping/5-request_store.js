03aeb18471b0592b2566b6793f8816a263ef9efb
Victor Hernandez <vik407@gmail.com>
2020-05-13 00:59:41 +0000

    Get content and store to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// Write a script that gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+
+request(`${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const _body = body;
+    fs.writeFile(`./${process.argv[3]}`, _body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
