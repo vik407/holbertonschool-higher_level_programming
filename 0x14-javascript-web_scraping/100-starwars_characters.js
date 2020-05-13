b016df529021e5d637c99c407adfe8d6d55be307
Victor Hernandez <vik407@gmail.com>
2020-05-13 01:23:32 +0000

    advanced minus twitter auth (no access)

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+// Write a script that prints all characters of a Star Wars movie
+const request = require('request');
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chars = JSON.parse(body).characters;
+    chars.forEach(char => {
+      request(char, (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+// Write a script that prints all characters of a Star Wars movie
+const request = require('request');
+
+const req = (arr, i) => {
+  if (i === arr.length) { return; }
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chars = JSON.parse(body).characters;
+    req(chars, 0);
+  }
+});
