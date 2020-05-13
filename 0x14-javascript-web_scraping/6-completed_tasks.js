225d7ad4ddae399548eadb4cd9b58d2db70021b1
Victor Hernandez <vik407@gmail.com>
2020-05-13 01:11:46 +0000

    Completed tasts by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+// Write a script that computes the number of tasks completed by user id.
+const request = require('request');
+
+request(`${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const arr = JSON.parse(body);
+    console.log(arr.reduce((obj, el) => {
+      if (el.completed) {
+        obj[el.userId]
+          ? obj[el.userId]++
+          : obj[el.userId] = 1;
+      }
+      return obj;
+    }, {}));
+  }
+});
