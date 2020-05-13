1b732724fe2d35406f4c123468003f2d2fe74fee
Victor Hernandez <vik407@gmail.com>
2020-05-13 00:56:15 +0000

    write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Write a script that writes a string to a file.
+const fs = require('fs');
+
+fs.writeFile(`${process.argv[2]}`, `${process.argv[3]}`, 'utf8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
